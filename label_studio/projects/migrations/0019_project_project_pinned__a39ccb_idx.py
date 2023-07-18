0a59c00a3fe60900d8f84a478de53482c8508f8e
hadiyouness10 <hadi.youness@lau.edu>
2023-07-18 13:58:06 +0200

    fixes

--- label_studio/projects/migrations/0019_project_project_pinned__a39ccb_idx.py
+++ label_studio/projects/migrations/0019_project_project_pinned__a39ccb_idx.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0018_alter_project_control_weights'),
+        ('projects', '0019_labelstreamhistory'),
     ]
 
     operations = [
